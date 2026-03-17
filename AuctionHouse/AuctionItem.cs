3de3dfa8f672e69664eddab695f2e7021d7c6aac
YunSangJin <sangjin411@gmail.com>
2026-03-17 14:00:00 +0900

    윤상진
    AuctionHoues 완

---
+++ AuctionHouse/AuctionItem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class AuctionItem
+{
+    public string Name { get; private set; }
+    public int CurrentBid{ get; private set; }
+    public int BidCount { get; private set; }
+    public string Category { get; private set; }
+
+    public AuctionItem(string name, int currentBid, int bidCount, string category)
+    {
+        Name = name;
+        CurrentBid = currentBid;
+        BidCount = bidCount;
+        Category = category;
+    }
+
+    public override string ToString()
+    {
+        return $"{Name} [{Category}] - 입찰가: {CurrentBid}골드 (입찰 {BidCount})";
+    }
+}
+class BidComparer : Comparer<AuctionItem>
+{
+    public override int Compare(AuctionItem x, AuctionItem y)
+    {
+        if (x == null && y == null)
+        {
+            return 0;
+        }
+        if (x == null)
+        { 
+            return -1;
+        }
+        if (y == null)
+        {
+            return 1;
+        }
+        return y.CurrentBid.CompareTo(x.CurrentBid);
+    }
+}
--- AuctionHouse/Program.cs
+++ AuctionHouse/Program.cs
@@ -1,4 +1,26 @@
 using System;
-
-// README.md를 읽고 코드를 작성하세요.
+using System.Collections.Generic;
+List<AuctionItem> items = new List<AuctionItem>
+{
+    new AuctionItem("전설의 검",50000,12,"무기"),
+    new AuctionItem("불꽃 반지",28000,15,"장신구"),
+    new AuctionItem("미스릴 갑옷",35000,8,"방어구"),
+    new AuctionItem("만능 물약",5000,20,"소비"),
+    new AuctionItem("회복 물약",5000,3,"소비"),
+};
+Console.WriteLine("=== 입찰가 기준 정렬 (BidComparer) ===");
+items.Sort(new BidComparer());
+foreach (AuctionItem item in items)
+{ 
+    Console.WriteLine(item);
+}
 Console.WriteLine();
+Console.WriteLine("=== 입찰 쇳수 기준 정렬 (Create 람다) ===");
+Comparer<AuctionItem> comparer = Comparer<AuctionItem>.Create(
+        (x, y) => y.BidCount.CompareTo(x.BidCount)
+    );
+items.Sort(comparer);
+foreach (AuctionItem t in items)
+{
+    Console.WriteLine(t);
+}
